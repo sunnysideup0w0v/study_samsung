385b8d08349f9022e059fc80e17f9dd0e28878c6
sunnysideup0w0 <sunnysideup0w0@gmail.com>
2020-04-29 16:36:32 +0900

    수정!

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-var gnb =$("#gnb .list > li");
-var header= $("#header"); 
-gnb.on("mouseenter", function(){
-    header.addClass("open");
-});
-gnb.on("mouseleave", function(){
-    header.removeClass("open");
-});
+var gnb = document.querySelectorAll("#gnb .list .depth01");
+var header= document.querySelector("#header"); 
+
+// gnb.addEventListenr("mouseenter", function(){
+//     header.addClass("open");
+// });
+// gnb.addEventListenr("mouseleave", function(){
+//     header.removeClass("open");
+// });
+// 수정 전
+
+gnb.addEventListener('mouseover',function(){
+    header.classList.add('open');
+})
+gnb.addEventListener('mouseout',function(){
+    header.classList.remove('open');
+})
+// 수정 후
+
+
+
 new fullpage('#main', {
 
 })
